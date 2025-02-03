ec33dadc5bba841ad2373d8ab9a6bbb916e3bc06
Darnitto001 <danielwamela@gmail.com>
2025-02-03 13:21:49 +0300

    extra commit

---
+++ introduction.py
@@ -0,0 +1,6 @@
+#Print is a method used to display a value
+
+print("Darnitto")
+print("Emobilis")
+print(39)
+print(100*2)
---
+++ variables.py
@@ -0,0 +1,17 @@
+#Variable is a container used to store a value
+
+number = 10
+firstname = "Darnitto"
+lastname = "Dante"
+language = "Python"
+a = 10
+b = 15
+
+print(number)
+print(firstname)
+print(lastname)
+print(firstname, lastname)
+print(language, "Is a web programming language")
+print(a + b)
+# The sum of 10 and 15 is 25
+print("The sum of a + b = ", a + b)
